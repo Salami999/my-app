65c645d4f8944a1b4449f996bd02be4178c83071
Salami999 <78253544+Salami999@users.noreply.github.com>
2021-02-08 14:09:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                try
+                {
+                    Console.WriteLine("Hell User");
+                    Console.WriteLine("Input Miles driven");
+                    double Mile = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter the gallon used");
+                    double Gallon = Convert.ToDouble(Console.ReadLine());
+
+                    double Calculate = (Mile / Gallon);
+
+                    Console.WriteLine("Miles per Gallon = " + Calculate + " MPG ");
+
+                    Console.ReadLine();
+                }
+
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                     Console.ReadLine();
+            }
+
+                
+
+
+
+            }
+        }
+    }
